f27e1b1f804318acf711dc6d1a5f50ad79f79972
NickCMoore <moorenicholasc@gmail.com>
2023-11-10 10:32:10 +0000

    Activate exit option from rules and add blur to rules background

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,15 @@
 const startButton = document.querySelector('.start-button');
 const rulesDisplay = document.querySelector('.rules-display');
+const quitButton = document.querySelector('.quit-button');
+const main = document.querySelector('.main');
 
 startButton.onclick = () => {
     rulesDisplay.classList.add('active');
+    main.classList.add('active');
 }
+
+quitButton.onclick = () => {
+    rulesDisplay.classList.remove('active');
+    main.classList.remove('active');
+}
+
