7021fac31178c1d8a199cc44d6e8d2aa3d8838da
NickCMoore <moorenicholasc@gmail.com>
2023-11-10 15:17:51 +0000

    Further work on button event handlers

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+// DOM element selection
 const startButton = document.querySelector('.start-button');
 const rulesDisplay = document.querySelector('.rules-display');
 const quitButton = document.querySelector('.quit-button');
@@ -5,24 +6,57 @@ const main = document.querySelector('.main');
 const continueButton = document.querySelector('.continue-button');
 const questionDisplay = document.querySelector('.question-display')
 const questionQuitButton = document.querySelector('.question-quit-button')
+const nextButton = document.querySelector('.next-button');
+const resultsDisplay = document.querySelector('.results-display')
+const exitButton = document.querySelector('.exit-button')
+const tryAgainButton = document.querySelector('.try-again-button')
 
+//Event handlers
 
+// Shows game rules
 startButton.onclick = () => {
     rulesDisplay.classList.add('active');
     main.classList.add('active');
 }
 
+// Quits to home screen
 quitButton.onclick = () => {
     rulesDisplay.classList.remove('active');
     main.classList.remove('active');
 }
 
+// Starts quiz
 continueButton.onclick = () => {
     questionDisplay.classList.add('active');
     rulesDisplay.classList.remove('active');
 }
 
+// Quits to home screen
 questionQuitButton.onclick = () => {
     questionDisplay.classList.remove('active')
     main.classList.remove('active');
+}
+
+// Moves to next question
+nextButton.onclick = () => {
+    questionDisplay.classList.remove('active')
+    resultsDisplay.classList.add('active');
+}
+
+// Shows player final score
+resultsDisplay.onclick = () => {
+    questionDisplay.classList.remove('active');
+    resultsDisplay.classList.add('active')
+}
+
+// Quits to home screen
+exitButton.onclick = () => {
+    resultsDisplay.classList.remove('active');
+    main.classList.add('active');
+}
+
+// Returns player to first question in quiz
+tryAgainButton.onclick = () => {
+    resultsDisplay.classList.remove('active');
+    questionDisplay.classList.add('active');
 }
